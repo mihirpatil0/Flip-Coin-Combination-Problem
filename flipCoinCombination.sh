e749cc07f8235ad53c576184f284ced6a5eea330
mihirpatil0 <mihirrpatil@gmail.com>
2021-05-26 23:51:02 +0530

    display Welcome message

---
+++ flipCoinCombination.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo -e "Welcome to Flip Coin Simulation\nThis problem displays winning percentage of Head or Tail\nCombination in a Singlet, Doublet and Triplet"
+echo "--------------------------------------------"
