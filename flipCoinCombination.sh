198ab9ec044bc3ff8b10e1cf4c8cbb17e4adaf76
mihirpatil0 <mihirrpatil@gmail.com>
2021-05-26 23:55:08 +0530

    Display is it head or tail

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -2,3 +2,11 @@
 
 echo -e "Welcome to Flip Coin Simulation\nThis problem displays winning percentage of Head or Tail\nCombination in a Singlet, Doublet and Triplet"
 echo "--------------------------------------------"
+
+Random=$((RANDOM%2))
+if [ $Random -eq 1 ]
+then
+	echo "IT IS HEAD"
+else
+	echo "IT IS TAI	L"
+fi
