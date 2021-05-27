2e0864d589cb2ef6408051a358fb9ba7bd0de9c8
mihirpatil0 <mihirrpatil@gmail.com>
2021-05-27 13:41:28 +0530

    Sorted the all three types of combination and printed the highest score

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -3,20 +3,26 @@
 echo -e "Welcome to Flip Coin Simulation\nThis problem displays winning percentage of Head or Tail\nCombination in a Singlet, Doublet and Triplet"
 echo "--------------------------------------------"
 
-declare -A Coin
+declare -A Dict
 read -p "Enter the number to flip a coin : " num
 
 function Case()
 {
 	case $1 in
-		111) echo "HHH";;
-		110) echo "HHT";;
-		101) echo "HTH";;
-		100) echo "HTT";;
-		011) echo "THH";;
-		010) echo "THT";;
-		001) echo "TTH";;
-		000) echo "TTT";;
+		1) echo "H" ;;
+        	0) echo "T" ;;
+        	11) echo "HH" ;;
+        	10) echo "HT" ;;
+        	01) echo "TH" ;;
+        	00) echo "TT" ;;
+        	111) echo "HHH" ;;
+        	110) echo "HHT" ;;
+        	101) echo "HTH" ;;
+        	100) echo "HTT" ;;
+        	011) echo "THH" ;;
+        	010) echo "THT" ;;
+        	001) echo "TTH" ;;
+	        000) echo "TTT" ;;
 
 	esac
 }
@@ -24,12 +30,41 @@ function Case()
 
 for (( i=1; i<=$num; i++ ))
 do
-	Random=$((RANDOM%2))$((RANDOM%2))$((RANDOM%2))
-	((++Coin[`Case $Random`]))
-done
+	Random=$((RANDOM%3+1))
+	if [ $Random -eq 1 ]
+	then
+		ran1=$((RANDOM%2))
+                ((++Dict[`Case $ran1`]))
+        elif [ $Random -eq 2 ]
+        then
+                ran2=$((RANDOM%2))$((RANDOM%2))
+                ((++Dict[`Case $ran2`]))
+        else
+                ran3=$((RANDOM%2))$((RANDOM%2))$((RANDOM%2))
+                ((++Dict[`Case $ran3`]))
+        fi
 
-for Key in ${!Coin[@]}
-do
-	Percntg=$(awk "BEGIN{print (${Coin[$Key]}/$num * 100 )}")
-	echo "Percentage of winning $Key is $Percntg %"
 done
+i=0
+for key in ${!Dict[@]}
+do
+        val=$(awk "BEGIN{print (${Dict[$key]}/$num * 100)}")
+        echo "Percentage of Winning $key is $val"
+        array[i++]=$(awk "BEGIN{print (${Dict[$key]}/$num * 100)}")
+done
+i=0
+echo ${array[@]}
+for ((i=0; i<num; i++))
+do
+  for ((j=0; j<i; j++))
+  do
+    if [[ ${array[$i]} -gt ${array[$j]} ]]
+    then
+        temp=${array[$j]}
+        array[$j]=${array[$i]}
+        array[$i]=$temp
+    fi
+  done
+done
+echo -e "Ascending order of combinations\n${array[@]}"
+echo ${array[0]} is the highest value
